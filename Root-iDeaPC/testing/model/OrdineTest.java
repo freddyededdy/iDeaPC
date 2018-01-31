f7028ab7db5f7defbadadb3c402a2f70554face3
Aquilino94 <32454883+Aquilino94@users.noreply.github.com>
2018-01-31 09:47:13 +0100

    modificati metodi in test

--- Root-iDeaPC/testing/model/OrdineTest.java
+++ Root-iDeaPC/testing/model/OrdineTest.java
@@ -11,7 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class OrdineTest {
-	Ordine ordine;
+	private Ordine ordine;
 	@Before
 	public void setUp() throws Exception {
 		ordine  = new Ordine();
--- Root-iDeaPC/testing/model/ProdottoTest.java
+++ Root-iDeaPC/testing/model/ProdottoTest.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ProdottoTest {
-	Prodotto prodotto;
+	private Prodotto prodotto;
 	@Before
 	public void setUp() throws Exception {
 		prodotto = new Prodotto();
